ef77542745e6fe4aab77a68534c84ec7ddd7958d
Eugene Chaikin <nutaksho@gmail.com>
2025-05-16 15:00:03 +0200

    Initialize empty option value if none present
    This way when we are redirected after create, the form will already
    have a field to enter option value data

--- admin/app/components/solidus_admin/option_types/edit/component.rb
+++ admin/app/components/solidus_admin/option_types/edit/component.rb
@@ -2,4 +2,9 @@
 
 class SolidusAdmin::OptionTypes::Edit::Component < SolidusAdmin::Resources::Edit::Component
   include SolidusAdmin::Layout::PageHelpers
+
+  def initialize(option_type)
+    super
+    option_type.option_values.build if option_type.option_values.empty?
+  end
 end
