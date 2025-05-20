94cdbee4f189c1ad6288caa7d5d63403587c2d7c
Eugene Chaikin <nutaksho@gmail.com>
2025-05-20 16:20:31 +0200

    Extend Row component
    Allow for additional attributes to be passed to the component in order
    to pass them down to an underlying <td> element.

--- admin/app/components/solidus_admin/ui/table/row/component.rb
+++ admin/app/components/solidus_admin/ui/table/row/component.rb
@@ -11,6 +11,8 @@ class SolidusAdmin::UI::Table::Row::Component < SolidusAdmin::BaseComponent
     @columns.map do |column|
       cell = column.data
       cell = cell.call(@row_object) if cell.respond_to?(:call)
+      cell, attributes = cell
+      attributes ||= {}
       cell = @row_object.public_send(cell) if cell.is_a?(Symbol)
       cell = cell.render_in(self) if cell.respond_to?(:render_in)
       cell = tag.div(cell, class: "flex items-center gap-1.5 justify-start overflow-x-hidden") if column.wrap
@@ -18,7 +20,8 @@ class SolidusAdmin::UI::Table::Row::Component < SolidusAdmin::BaseComponent
       tag.td(cell, class: "
         py-2 px-4 h-10 vertical-align-middle leading-none
         [tr:last-child_&:first-child]:rounded-bl-lg [tr:last-child_&:last-child]:rounded-br-lg
-      ")
+        #{attributes.delete(:class)}
+      ", **attributes)
     end
   end
 end
